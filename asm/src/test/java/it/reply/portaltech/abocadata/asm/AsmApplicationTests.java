066af1c3d9f4f08bca729abec1f4bcbb171f8153
nguerriero <n.guerriero@reply.it>
2021-02-23 10:26:23 +0100

    asm_develop - init project setup

---
+++ asm/src/test/java/it/reply/portaltech/abocadata/asm/AsmApplicationTests.java
@@ -0,0 +1,13 @@
+package it.reply.portaltech.abocadata.asm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
